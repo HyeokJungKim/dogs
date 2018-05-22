ba90dc7271f35f61c2008e9fd43ec9d1410bcb18
Eric Kim <hyeokjungkim@gmail.com>
2018-05-22 17:28:58 -0400

    Changed back to class component

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,12 +1,21 @@
 import React,{Component} from 'react'
 
+class Dog extends Component{
+  state = {
+    clicked: false
+  }
 
-const Dog = (props) => {
-  return(
-    <div>
-      <img src={props.link}></img>
-    </div>
-  )
+  makeFavorite = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
+
+  render(){
+    return(
+      <div onClick={this.makeFavorite}>
+        {this.state.clicked ? <h1>"Saved"</h1> : <img src={this.props.link}></img>}
+      </div>
+    )
+  }
 }
 
 export default Dog;
